474aad84d49328789277e3680b7d1370a9879a88
Aditya Bhardwaj <30766241+wpboi@users.noreply.github.com>
2017-08-09 16:12:31 +0530

    Section 1 - What can JS do?
    #Udemy

---
+++ myScript.js
@@ -0,0 +1,19 @@
+function brutus() {
+	document.getElementById('murder').innerHTML='Ahh. Et Tu, Brute....'
+}
+
+function switchOn() {
+	document.getElementById('myImage').src='http://i.imgur.com/2IRs7Bs.gif'
+}
+
+function switchOff() {
+	document.getElementById('myImage').src='http://i.imgur.com/8kGmXwB.gif'
+}
+
+function magic() {
+	document.getElementById('magic').style.display='none'
+}
+
+function mtrue() {
+	document.getElementById('magic').style.display='block'
+}
